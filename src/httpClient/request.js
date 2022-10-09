e3d8f78b763b063c680ce9205463764de5ff0779
owenlai <owenlai@xiaoe-tech.com>
2022-10-09 23:31:17 +0800

    修改headerIP

--- src/httpClient/request.js
+++ src/httpClient/request.js
@@ -2,9 +2,17 @@
 import axios from "axios";
 import qs from "qs"
 axios.defaults.headers["Content-Type"] = 'application/json;charset=utf-8'
+const IP = '192.168.0.91'
 const service = axios.create({
     baseURL: '',
-    timeout: 30000
+    timeout: 30000,
+    headers: {
+        'X-Forwarded-For': IP,
+        'X-Originating-IP': IP,
+        'X-Remote-IP': IP,
+        'X-Remote-Addr': IP,
+        'X-Client-IP': IP
+    }
 })
 
 service.interceptors.request.use(config => {
